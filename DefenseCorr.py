63ad946938e5b8e4479b978a6c4eb4346e7dfdc6
ryanburd <ryankburdick@gmail.com>
2023-02-28 23:58:12 -0500

    Finished reformatting code for better OOP and scalability

--- DefenseCorr.py
+++ DefenseCorr.py
@@ -1,11 +1,13 @@
 # This code uses Pandas dataframes to analyze the correlation strength of standard defensive stats to advanced metric defensive stats. The goal is to determine which standard fielding stat(s) best correlates to the advanced metric outs above average (OAA), widely regarded as the best defensive metric for baseball players today.
 #
-# Standard stats (abbrev.): assists (A), assists from outfield (AOF), putouts (PO), errors (E), fielding percentage (FLD%), double plays (DP), caught stealing against (CSA), stolen bases allowed (SBA), games played (GP)
+# Standard stats (abbrev.): assists (A), assists from outfield (AOF), putouts (PO), errors (E), fielding percentage (FLD%), double plays (DP), games played (GP)
 #
-# All data is from 2022 Major League Baseball (MLB) games. OAA data is from Baseball Savant. Standard fielding data are from Fantrax. Only qualified fielders (defined by Baseball Savant as having played 1 inning in the field per team game played) are used in correlation calculations.
+# All data is from 2022 Major League Baseball (MLB) games. OAA data is from Baseball Savant. Standard fielding data are from Fantrax. Only data from qualified fielders are used in correlation calculations, as defined by Baseball Savant. For 2B, SS, 3B, OF: 1 fielding attempt per team game played. For 1B: 1 fielding attempt per every other team game played. Catchers do not have OAA and so are not included here.
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from math import ceil
+from unidecode import unidecode
 
 def read_data(file_name):
     data = pd.read_csv(file_name)
@@ -13,8 +15,8 @@ def read_data(file_name):
     return data
 
 def reformat_OAA_df(df, desired_stats):
-    # For an OAA csv, remove the space in front of the first name and combine it with the last name as one new column. Rename columns for easier reading.
-    df['Player'] = df[' first_name'].map(lambda x: x.lstrip(' ')) + " " + df['last_name']
+    # For an OAA csv, remove the space in front of the first name and combine it with the last name as one new column. Change any accented letters to the closest unaccented letter. Rename columns for easier reading.
+    df['Player'] = df[' first_name'].map(lambda x: unidecode(x.lstrip(' '))) + " " + df['last_name'].map(lambda x: unidecode(x))
     df = df.rename(columns={'primary_pos_formatted' : 'Pos'})
     df = df.rename(columns={'outs_above_average' : 'OAA'})
 
@@ -24,7 +26,8 @@ def reformat_OAA_df(df, desired_stats):
     return df
 
 def reformat_std_df(df, desired_stats):
-    # For the standard fielding csv, remove the quotes around the player name. Remove the thousands separator from PO and recast to int.
+    # For the standard fielding csv, only include players with at least 1 game played (this removes prospects not in the MLB who may have the same name as an MLB player). Remove the quotes around the player name. Remove the thousands separator from all stats and recast to int.
+    df = df[df['GP'] > 0]
     df['Player'] = df['Player'].map(lambda x: x.lstrip('"').rstrip('"'))
     df = df.replace(',','', regex=True)
     df['PO'] = df['PO'].astype(int)
@@ -40,7 +43,7 @@ def merge_OAA_std(OAA_df, std_df, sort_stat):
 
     return merged_df
 
-class PositionDf:
+class PositionGroup:
 
     # Create a list of all positions contained within the group. Select only those positions from the all positions dataframe.
     def __init__(self, positions, allPos_df):
@@ -48,37 +51,45 @@ class PositionDf:
         self.df = allPos_df[allPos_df['Pos'].isin(self.positions)]
 
         # If no outfield positions are included, remove AOF (outfield only stat).
-        if any(position in self.positions for position in ['LF', 'CF', 'RF']):
+        if not any(position in self.positions for position in ['LF', 'CF', 'RF']):
             self.df = self.df.drop(columns=['AOF'])
-
-        # If catchers are not included, remove CSA and SBA (catcher only stats).
-        if 'C' not in self.positions:
-            self.df = self.df.drop(columns=['CSA', 'SBA'])
     
     def calculate_corr(self):
         self.corr_matrix = self.df.corr()
     
-    def plot_corr_coeff(self, corr_stat):
+    def plot_corr_coeff(self, corr_stat, ax):
         # Remove the stat to which all others should be correlated (corr_stat). It's going to have a correlation coefficient of 1 with itself.
         self.corr_matrix = self.corr_matrix.drop(corr_stat)
 
         # Plot the other stats' correlation coefficient with corr_stat.
-        fig, ax = plt.subplots(nrows=1, ncols=1)
         self.corr_matrix[[corr_stat]].plot(ax=ax, kind='bar')
         ax.set_title(', '.join(self.positions))
         ax.set_xlabel('Defensive stat')
         ax.set_ylabel('Correlation coefficient w/ %s' % corr_stat)
-        plt.show()
 
-# def plot_corr_coeff(corr_matrix, corr_stat):
+def plot_position_groups(groups, df):
+    num_groups = len(groups)
+    num_rows = 2
+    num_cols = ceil(num_groups/num_rows)
+    fig, axes = plt.subplots(nrows=num_rows, ncols=num_cols, sharey=True)
 
-#     fig1, axes = plt.subplots(nrows=1, ncols=3, sharey=True)
+    group_index = 0
+    for row in range(num_rows):
+        for col in range(num_cols):
+            if group_index < num_groups:
+                # Create a dataframe containing only the positions within the group
+                PG = PositionGroup(groups[group_index], df)
+                group_index += 1
 
-#     corr_matrix = corr_matrix.drop(corr_stat)
-#     corr_matrix[[corr_stat]].plot(ax=axes[0], kind='bar')
-#     axes[0].set_title('All positions')
-#     # axes[0].set_xlabel('Standard defensive stat')
-#     axes[0].set_ylabel('Correlation coefficient')
+                # Calculate correlation matrix of the dataframe.
+                PG.calculate_corr()
+
+                # Create a bar graph of the correlation coefficients for each stat with the desired stat, corr_stat.
+                corr_stat = 'OAA'
+                PG.plot_corr_coeff(corr_stat, axes[row, col])
+    
+    plt.tight_layout()
+    plt.show()
 
 def main():
 
@@ -90,48 +101,24 @@ def main():
     desired_OAA_stats = ['Player', 'Pos', 'OAA']
     OAA_df = reformat_OAA_df(OAA_df, desired_OAA_stats)
 
-    desired_std_stats = ['Player', 'A', 'AOF', 'PO', 'E', 'FLD%', 'DP', 'CSA', 'SBA', 'GP']
+    desired_std_stats = ['Player', 'A', 'AOF', 'PO', 'E', 'FLD%', 'DP', 'GP']
     std_df = reformat_std_df(std_df, desired_std_stats)
 
     # Merge the OAA and standard stats into one dataframe. Sort the merged dataframe by the desired stat.
     sort_stat = 'OAA'
     merged_df = merge_OAA_std(OAA_df, std_df, sort_stat)
 
-    # Create a position grouping for correlation calculations. Create a dataframe containing only those positions.
-    positions = ['C', '1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
-    all_positions = PositionDf(positions, merged_df)
+    # Create a figure showing the correlation coefficients between OAA and standard defensive stats for different position groups.
+    all_positions = ['1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF']
+    infield = ['1B', '2B', '3B', 'SS']
+    infield_no1B = ['2B', '3B', 'SS']
+    outfield = ['LF', 'CF', 'RF']
+    groups = [infield, infield_no1B, outfield, all_positions]
+    plot_position_groups(groups, merged_df)
 
-    # Calculate correlation matrix of the merged dataframe.
-    all_positions.calculate_corr()
-
-    # Create a bar graph of the correlation coefficients for each stat with the desired stat, corr_stat.
-    corr_stat = 'OAA'
-    all_positions.plot_corr_coeff(corr_stat)
+    # Create a figure of the correlation coefficients for each position individually.
+    individual_positions = [['1B'], ['2B'], ['3B'], ['SS'], ['LF'], ['CF'], ['RF']]
+    plot_position_groups(individual_positions, merged_df)
 
 if __name__ == "__main__":
     main()
-
-# def by_position():
-#     ncols = 4
-#     fig2, axes = plt.subplots(nrows=2, ncols=ncols, sharey=True)
-#     pos = ['1B', '2B', '3B', 'SS', 'LF', 'CF', 'RF', 'C']
-
-#     for i in range(len(pos)):
-#         pos_stats = merged_stats[merged_stats['Pos'].isin([pos[i]])]
-#         if pos[i] in infield:
-#             pos_stats = pos_stats.drop(columns=['AOF', 'CSA', 'SBA'])
-#         elif pos[i] in outfield:
-#             pos_stats = pos_stats.drop(columns=['A', 'DP', 'CSA', 'SBA'])
-#         pos_corr = pos_stats.corr()
-#         pos_corr = pos_corr.drop('OAA')
-#         if i < ncols:
-#             pos_corr[['OAA']].plot(ax=axes[0,i], kind='bar')
-#             axes[0,i].set_title(pos[i])
-#         else:
-#             pos_corr[['OAA']].plot(ax=axes[1,i-ncols], kind='bar')
-#             axes[1,i-ncols].set_title(pos[i])
-
-    
-#     plt.show()
-
-# by_position()
